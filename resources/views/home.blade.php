9b4c674a4cee523c2372d2c91f0d573529299902
oriko <ori.kou.1025@gmail.com>
2023-08-23 12:10:49 +0900

    home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,14 +12,6 @@
         </li>
     </x-slot>
 
-
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('投稿一覧') }}
-        </h2>
-    </x-slot>
-
     <div class="max-w-7xl mx-auto mt-10 px-4 sm:px-6 lg:px-8">
         <div class="my-4">
             <a href="{{ route('post.create') }}" class="inline-block py-2 px-4 btn btn-primary text-decoration-none">
@@ -52,9 +44,7 @@
             @endif
         </div>
     </div>
-    
-</x-app-layout>
-    <div class="py-12">
+        <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <a href="{{ route('post.index') }}" class="bg-white border-b border-gray-200 p-6 block w-full text-center
