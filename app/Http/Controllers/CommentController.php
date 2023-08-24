98ff7964a763eea919f8b741a21c19bee53ebf41
MIST1216 <taishi2002i@gmail.com>
2023-08-24 16:45:09 +0900

    コメント機能の完成

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -3,51 +3,47 @@
 namespace App\Http\Controllers;
 
 use App\Models\Comment;
+use App\Models\TrafficAccident;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class CommentController extends Controller
 {
-    public function index()
+    public function index($id)
     {
-        $comments = Comment::orderBy('updated_at', 'desc')->get();
-        return view('comment.index', compact('comments'));
+        $comments = Comment::where('traffic_accident_id', $id)->orderBy('updated_at', 'desc')->get();
+        return view('comment.index', compact('comments', 'id'));
     }
     
-    public function create()
+    public function create($id)
     {
-        return view('comment.create');
+        return view('comment.create', compact(('id')));
     }
 
-    public function store(Request $request)
+    public function store(Request $request, $id)
     {
         $validatedData = $request->validate([
             'comment_text' => 'required|string',
-            'total_good' => 'required|int',
-            'total_bad' => 'required|int',
-            'traffic_accident_id' => 'required|int',
         ]);
 
         $comment = new Comment();
         $comment->comment_text = $validatedData['comment_text'];
-        $comment->total_good = $validatedData['total_good'];
-        $comment->total_bad = $validatedData['total_bad'];
         $comment->user_id = Auth::id();
-        $comment->traffic_accident_id = $validatedData['traffic_accident_id'];
+        $comment->traffic_accident_id = $id;
         $comment->save();
 
-        return redirect()->route('comment.index')->with('success', '投稿が作成されました');
+        return redirect()->route('comment.index', ['id'=>$id])->with('success', '投稿が作成されました');
     }
 
-    public function myComment()
+    public function myComment($id)
     {
-        $comments = Comment::where('user_id', Auth::id())->orderBy('updated_at', 'desc')->get();
+        $comments = Comment::where('user_id', Auth::id())->where('traffic_accident_id', $id)->orderBy('updated_at', 'desc')->get();
         return view('my-comments', compact('comments'));
     }
 
     public function edit($id)
     {
-        $post = Comment::findOrFail($id);
+        $comment = Comment::findOrFail($id);
         return view('comment.edit', compact('comment'));
     }
 
@@ -55,24 +51,22 @@ class CommentController extends Controller
     {
         $validatedData = $request->validate([
             'comment_text' => 'required|string',
-            'total_good' => 'required|int',
-            'total_bad' => 'required|int',
-            'traffic_accident_id' => 'required|int',
         ]);
 
         $comment = Comment::findOrFail($id);
         $comment->comment_text = $validatedData['comment_text'];
+        $tr_id = $comment->traffic_accident_id;
         $comment->save();
-
-        return redirect()->route('mycomments')->with('success', '投稿が更新されました');
+        return redirect()->route('mycomments',['id'=>$tr_id])->with('success', '投稿が更新されました');
     }
 
     public function destroy($id)
     {
         $comment = Comment::findOrFail($id);
+        $tr_id = $comment->traffic_accident_id;
         $comment->delete();
 
-        return redirect()->route('mycomments')->with('success', '投稿が削除されました');
+        return redirect()->route('mycomments',['id'=>$tr_id])->with('success', '投稿が削除されました');
     }
 }
 
--- resources/views/comment/edit.blade.php
+++ resources/views/comment/edit.blade.php
@@ -4,29 +4,15 @@
             <div class="col-md-8">
                 <div class="card">
                     <div class="card-body mt-4">
-                        <form method="COMMENT" action="{{ route('comment.update', $comment->id) }}">
+                        <form method="post" action="{{ route('comment.update', $comment->id) }}">
                             @csrf
                             @method('PATCH')
 
-                            <div class="form-group row">
-                                <label for="title" class="col-md-4 col-form-label text-md-right">{{ __('タイトル') }}</label>
-
-                                <div class="col-md-6">
-                                    <input id="title" type="text" class="form-control @error('title') is-invalid @enderror" name="title" value="{{ old('title', $comment->title) }}" required autocomplete="title" autofocus>
-
-                                    @error('title')
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $message }}</strong>
-                                        </span>
-                                    @enderror
-                                </div>
-                            </div>
-
                             <div class="form-group row my-4">
-                                <label for="body" class="col-md-4 col-form-label text-md-right">{{ __('本文') }}</label>
+                                <label for="comment_text" class="col-md-4 col-form-label text-md-right">{{ __('コメント') }}</label>
 
                                 <div class="col-md-6">
-                                    <textarea id="body" class="form-control @error('body') is-invalid @enderror" name="body" required>{{ old('body', $comment->body) }}</textarea>
+                                    <textarea id="comment_text" class="form-control @error('comment_text') is-invalid @enderror" name="comment_text" required>{{ old('comment_text', $comment->comment_text) }}</textarea>
 
                                     @error('body')
                                         <span class="invalid-feedback" role="alert">
