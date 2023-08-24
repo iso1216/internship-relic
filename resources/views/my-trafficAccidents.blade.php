53cfc38e1ac3186cd89dcb2e09c0bdae03bfebd5
oriko <ori.kou.1025@gmail.com>
2023-08-24 09:56:39 +0900

    全てのAccidentからTrafficAccidentに変更しました

---
+++ resources/views/my-trafficAccidents.blade.php
@@ -0,0 +1,39 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('事故登録一覧') }}
+        </h2>
+    </x-slot>
+
+    <div class="max-w-7xl mx-auto mt-10 sm:px-6 lg:px-8">
+        @if (!empty($trafficAccidents))
+            <div class="grid grid-cols-1 gap-4">
+                @foreach ($trafficAccidents as $trafficAccident)
+                    <div class="bg-white shadow p-6 rounded-lg">
+                        <h4 class="text-lg font-bold">{{ $trafficAccident->title }}</h4>
+                        <p class="text-gray-800">{{ $trafficAccident->body }}</p>
+                        <p class="text-gray-800">{{ $trafficAccident->updated_at }}</p>
+
+                        <div class="mt-4 flex">
+                            <a href="{{ route('trafficAccident.edit', ['id' => $trafficAccident->id]) }}" class="btn btn-primary mr-2"
+                                role="button">
+                                {{ __('編集') }}
+                            </a>
+                            <form action="{{ route('trafficAccident.destroy', ['id' => $trafficAccident->id]) }}" method="trafficAccident">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" class="btn btn-danger" onclick="return confirm('本当に削除しますか？')">
+                                    {{ __('削除') }}
+                                </button>
+                            </form>
+                        </div>
+                    </div>
+                @endforeach
+            </div>
+        @else
+            <div class="flex justify-center items-center h-full">
+                <p class="text-lg text-gray-600">登録された事故情報はありません</p>
+            </div>
+        @endif
+    </div>
+</x-app-layout>
---
+++ resources/views/trafficAccident/index.blade.php
@@ -0,0 +1,40 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('事故一覧') }}
+        </h2>
+    </x-slot>
+
+    <div class="max-w-7xl mx-auto mt-10 px-4 sm:px-6 lg:px-8">
+        <div class="my-4">
+            <a href="{{ route('trafficAccident.create') }}" class="inline-block py-2 px-4 btn btn-primary text-decoration-none">
+                {{ __('投稿する') }}
+            </a>
+
+            <a href="{{ route('mytrafficaccidents') }}" class="inline-block ml-4 py-2 px-4 btn btn-secondary text-decoration-none">
+                {{ __('自分の事故登録を確認する') }}
+            </a>
+        </div>
+
+        <div class="my-4">
+            @if (!empty($trafficAccidents))
+                <ul>
+                    @foreach ($trafficAccidents as $trafficAccident)
+                        <li class="mb-6 bg-white border rounded-lg p-4">
+                            <h3 class="text-lg font-bold mb-2 border-bottom">{{ $trafficAccident->title }}</h3>
+                            <p class="text-gray-1000 mt-4">{{ $trafficAccident->body }}</p>
+                            <div class="flex justify-between mt-8">
+                                <p class="text-gray-600">{{ $trafficAccident->user->name }}</p>
+                                <p class="text-gray-600">{{ $trafficAccident->updated_at }}</p>
+                            </div>
+                        </li>
+                    @endforeach
+                </ul>
+            @else
+                <div class="flex justify-center items-center h-full">
+                    <p class="text-lg text-gray-600">投稿はありません。</p>
+                </div>
+            @endif
+        </div>
+    </div>
+</x-app-layout>
