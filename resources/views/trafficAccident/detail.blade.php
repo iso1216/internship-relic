70367a4792ff41c2eee242acc9ca7a61c48d6126
MIST1216 <taishi2002i@gmail.com>
2023-08-24 18:51:34 +0900

    css設定

--- resources/views/trafficAccident/detail.blade.php
+++ resources/views/trafficAccident/detail.blade.php
@@ -8,15 +8,16 @@
     <div class="max-w-7xl mx-auto mt-10 sm:px-6 lg:px-8">
         @if (!empty($trafficAccident))
         <ul>
-            <li class="mb-6 bg-white border rounded-lg p-4">
-                <h3 class="text-lg font-bold mb-2 border-bottom">{{ $trafficAccident->user->name }}</h3>
-                <div class="flex justify-between mt-8">
-                    <p class="text-gray-600">{{ $trafficAccident->accident_place }}</p>
+            <div class="flex justify-center mb-6 bg-white border rounded-lg p-4">
+                <p class="text-lg font-bold mb-2">{{ $trafficAccident->user->name }}</p>
+                <p class="absolute right-48 text-lg">{{ $trafficAccident->accident_time }}</p>
+                <div class="mt-8">
+                    <p class="text-xl font-bold">{{ $trafficAccident->accident_place }}</p>
+                    <iframe src="https://maps.google.co.jp/maps?output=embed&q={{ $trafficAccident->accident_place }}" width="600" height="400" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    <label class="p-2 text-2xl font-bold text-gray-800">{{ __('詳細') }}</label>
                     <p class="text-gray-800">{{ $trafficAccident->accident_detail }}</p>
-                    <p class="text-lg">{{ $trafficAccident->accident_time }}</p>
-                    <iframe src="https://maps.google.co.jp/maps?output=embed&q={{ $trafficAccident->accident_place }}" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                 </div>
-            </li>
+            </div>
             <div class="absolute right-12">
                 <a href="{{ route('comment.index', ['id' => $trafficAccident->id]) }}" class="block w-full text-center font-semibold text-gray-800 hover:bg-gray-100 text-decoration-none">
                     <div class="inline-block rounded-lg bg-gray-300 hover:bg-gray-400 py-2 px-8 text-gray-800 hover:text-black">
