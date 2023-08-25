e3f85a34bea4d58a842748d3b58b86790c1e7954
MIST1216 <taishi2002i@gmail.com>
2023-08-25 10:35:33 +0900

    cssの調整

--- resources/views/trafficAccident/detail.blade.php
+++ resources/views/trafficAccident/detail.blade.php
@@ -8,13 +8,14 @@
     <div class="relative max-w-7xl mx-auto mt-10 sm:px-6 lg:px-16">
         @if (!empty($trafficAccident))
         <div class="flex justify-center mb-6 bg-white border rounded-lg p-4">
-            <p class="text-lg font-bold mb-2">{{ $trafficAccident->user->name }}</p>
+            <p class="absolute left-48 text-2xl font-bold mb-2">{{ $trafficAccident->user->name }}</p>
             <div class="mt-8">
+                <label class="pt-4 text-3xl font-bold text-gray-800">{{ __('事故発生場所') }}</label>
                 <p class="text-xl font-bold">{{ $trafficAccident->accident_place }}</p>
                 <iframe src="https://maps.google.co.jp/maps?output=embed&q={{ $trafficAccident->accident_place }}" width="600" height="400" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-                <label class="p-2 text-2xl font-bold text-gray-800">{{ __('詳細') }}</label>
-                <p class="text-gray-800">{{ $trafficAccident->accident_detail }}</p>
-                <label class="p-2 text-2xl font-bold text-gray-800">{{ __('発生時刻') }}</label>
+                <label class="pt-4 text-3xl font-bold text-gray-800">{{ __('詳細') }}</label>
+                <p class="text-gray-800 text-lg">{{ $trafficAccident->accident_detail }}</p>
+                <label class="pt-4 text-3xl font-bold text-gray-800">{{ __('発生時刻') }}</label>
                 <p class="text-xl">{{ $trafficAccident->accident_time }}</p>
             </div>
             <div class="absolute bottom-12 right-24">
